acc0aa9d7f8937103125bb5e9869e3382cf33f18
ksemanur <59751315+ksemanur8@users.noreply.github.com>
2021-10-31 23:16:02 -0400

    Feature tables complete

--- scripts/minitables.js
+++ scripts/minitables.js
@@ -1,5 +1,7 @@
+let minitable;
+
 $( document ).ready(function() {
-    $('#age-table').DataTable( {
+    minitable = $("#featured-table").DataTable( {
         ajax: {
             url: "age-averages.json",
             dataSrc: ""
@@ -15,36 +17,89 @@ $( document ).ready(function() {
         paging: true,
         pageLength: 7
     });
-    $('#gender-table').DataTable( {
-        ajax: {
-            url: "gender-averages.json",
-            dataSrc: ""
-        },
-        columns: [
-            {data: "Indicator"},
-            {type: "date",
-            data: "Time Period Label"},
-            {data: "Gender"},
-            {data: "Average Value"},
-        ], 
-        searching: false,
-        paging: true,
-        pageLength: 5
-    });
-    $('#race-table').DataTable( {
-        ajax: {
-            url: "race-averages.json",
-            dataSrc: ""
-        },
-        columns: [
-            {data: "Indicator"},
-            {type: "date",
-            data: "Time Period Label"},
-            {data: "Race/Hispanic Ethnicity"},
-            {data: "Average Value"},
-        ], 
-        searching: false,
-        paging: true,
-        pageLength: 5
-    });
 });
+
+$("#age-btn").click(function() {
+    if($("#gender-btn").hasClass("active") || $("#race-btn").hasClass("active")) {
+        $("#gender-btn").removeClass("active");
+        $("#race-btn").removeClass("active");
+        $("#age-btn").addClass("active");
+        minitable.destroy();
+        renderMiniTable("age");
+    }
+})
+
+$("#gender-btn").click(function() {
+    if($("#age-btn").hasClass("active") || $("#race-btn").hasClass("active")) {
+        $("#age-btn").removeClass("active");
+        $("#race-btn").removeClass("active");
+        $("#gender-btn").addClass("active");
+        minitable.destroy();
+        renderMiniTable("gender");
+    }
+})
+
+$("#race-btn").click(function() {
+    if($("#age-btn").hasClass("active") || $("#gender-btn").hasClass("active")) {
+        $("#age-btn").removeClass("active");
+        $("#gender-btn").removeClass("active");
+        $("#race-btn").addClass("active");
+        minitable.destroy();
+        renderMiniTable("race");
+    }
+})
+
+function renderMiniTable(type) {
+    if(type == "age") {
+        minitable = $("#featured-table").DataTable( {
+            ajax: {
+                url: "age-averages.json",
+                dataSrc: ""
+            },
+            columns: [
+                {data: "Indicator"},
+                {type: "date",
+                data: "Time Period Label"},
+                {data: "Age Group"},
+                {data: "Average Value"},
+            ], 
+            searching: false,
+            paging: true,
+            pageLength: 7
+        });
+    } else if(type == "gender") {
+        minitable = $("#featured-table").DataTable( {
+            ajax: {
+                url: "gender-averages.json",
+                dataSrc: ""
+            },
+            columns: [
+                {data: "Indicator"},
+                {type: "date",
+                data: "Time Period Label"},
+                {data: "Gender"},
+                {data: "Average Value"},
+            ], 
+            searching: false,
+            paging: true,
+            pageLength: 5
+        });
+    } else if(type == "race") {
+        minitable = $("#featured-table").DataTable( {
+            ajax: {
+                url: "race-averages.json",
+                dataSrc: ""
+            },
+            columns: [
+                {data: "Indicator"},
+                {type: "date",
+                data: "Time Period Label"},
+                {data: "Race/Hispanic Ethnicity"},
+                {data: "Average Value"},
+            ], 
+            searching: false,
+            paging: true,
+            pageLength: 5
+        });
+    }
+}
